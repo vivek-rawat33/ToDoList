b7f3afa9b5bd1d096a61d313074980a34f23b157
vivek_singh_rawat <viveksr012@gmail.com>
2024-12-30 15:25:28 +0530

    Initial commit

---
+++ to-do-list/script.js
@@ -0,0 +1,49 @@
+const input = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const button = document.querySelector("button");
+const alert = document.querySelector(".msg");
+const addTask = () => {
+  if (input.value === "") {
+    alert.textContent = "Please fill in the input box";
+    alert.style.color = "red";
+    alert.style.display = "block";
+  } else {
+    let li = document.createElement("li");
+    li.textContent = input.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+    input.value = "";
+    alert.style.display = "none";
+    msg.textContent = "";
+  }
+  saveData();
+};
+input.addEventListener("keydown", function () {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
+
+listContainer.addEventListener(
+  "click",
+  (e) => {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
